43fabb3c423e285ccebf193a720deea6558cd624
Albuquerque <alb.develloper@gmail.com>
2025-04-16 18:15:33 -0300

    feat: button hover colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,9 +18,9 @@ export const Button: FC<ButtonProps> = ({
   const className = useMemo(() => {
     switch (variant) {
       case "primary":
-        return "bg-black text-white dark:bg-white dark:text-black border-black dark:border-white";
+        return "bg-black text-white dark:bg-white dark:text-black border-black dark:border-white dark:hover:bg-black dark:hover:text-white hover:bg-white hover:text-black";
       case "secondary":
-        return "bg-transparent text-black dark:text-white border-black dark:border-white";
+        return "bg-transparent text-black dark:text-white border-black dark:border-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black";
       default:
         return "";
     }
@@ -32,7 +32,7 @@ export const Button: FC<ButtonProps> = ({
       className={cn(
         className,
         props.className,
-        "p-6 rounded-none border-2 cursor-pointer border-solid",
+        "p-6 rounded-none border-2 cursor-pointer border-solid transition-all",
       )}
     >
       {children}
